df7e3854ecd2f93f4928d2bb56c2e4546c3c9ad0
raphaeldenis <raphael.denis@coding_academy.fr>
2018-06-06 16:56:30 +0200

    correction default routing return ->render('@OCPlatform/Default/index.html.twig'); while adding a new module TEST OK

--- SymfonyOC/my_project/README.md
+++ SymfonyOC/my_project/README.md
@@ -1,4 +1,4 @@
 my_project
 ==========
 
-A Symfony project created on June 6, 2018, 3:17 pm.
+A Symfony project created on June 6, 2018, 4:28 pm.
--- SymfonyOC/my_project/src/OC/PlatformBundle/Controller/DefaultController.php
+++ SymfonyOC/my_project/src/OC/PlatformBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('OCPlatformBundle:Default:index.html.twig');
+        return $this->render('@OCPlatform/Default/index.html.twig');
     }
 }
