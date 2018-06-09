4b29c9bfa75233d53fcf316114ad7d81dad8f3e3
raphaeldenis <raphael.denis@coding_academy.fr>
2018-06-09 10:04:44 +0200

    adding const POKEMON and charging into model Pokemon, create event in component when user select a pokemon, display it

--- AngularOC/pokemon-app/app/app.component.ts
+++ AngularOC/pokemon-app/app/app.component.ts
@@ -1,7 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock.pokemon';
 
 @Component({
     selector:'pokemon-app',
-    template: '<h1>Bonjour, Angular 2!</h1>',
+    template: '<h1>Pokemon</h1>',
 })
-export class AppComponent { }
+export class AppComponent implements OnInit { 
+
+    //get data from external file
+    pokemons: Pokemon[] = null;
+
+    //on init, charge data from const POKEMONS to modele pokemons
+    ngOnInit() {
+        this.pokemons = POKEMONS;
+    }
+
+    //on event selectPokemon, display consolelog
+    selectPokemon(pokemon: Pokemon) {
+        console.log('VOus avez selectionné ' +pokemon.name);
+    }
+}
