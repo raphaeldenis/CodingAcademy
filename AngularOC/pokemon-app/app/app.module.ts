ca79a97f5d350d4a2e2c624b3087f633c8bd8e97
raphaeldenis <raphael.denis@coding_academy.fr>
2018-06-18 16:46:34 +0200

    routing NOK, added module for 404 management, module ok but app not displayed, pending

--- AngularOC/pokemon-app/app/app.module.ts
+++ AngularOC/pokemon-app/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListPokemonComponent } from './list-pokemon.component';
 import { DetailPokemonComponent } from './detail-pokemon.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+
 
 import { ShadowCardDirective} from './shadow-card.directive'; //import our directive
 import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
@@ -17,7 +19,8 @@ import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
         ShadowCardDirective,
         PokemonTypeColorPipe,
         ListPokemonComponent,
-        DetailPokemonComponent
+        DetailPokemonComponent,
+        PageNotFoundComponent
     ],
     bootstrap:    [ AppComponent ]
 })
