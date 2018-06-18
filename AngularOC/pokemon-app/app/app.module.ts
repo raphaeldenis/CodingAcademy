44269ebfffa7814a8341fffc56efe9863494bd34
raphaeldenis <raphael.denis@coding_academy.fr>
2018-06-18 12:01:39 +0200

    adding script for polyfill: mozilla doesnt display otherwise + error in color display, HTML elements are modified but color doesnt display, repair in progress

--- AngularOC/pokemon-app/app/app.module.ts
+++ AngularOC/pokemon-app/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ShadowCardDirective} from './shadow-card.directive'; //import our directive
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 
 @NgModule({
     imports:      [ BrowserModule ],
-    declarations: [ AppComponent, ShadowCardDirective ], //declare ShadowCardDirective in the root app module
+    declarations: [ AppComponent, ShadowCardDirective, PokemonTypeColorPipe ], //declare ShadowCardDirective in the root app module
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
