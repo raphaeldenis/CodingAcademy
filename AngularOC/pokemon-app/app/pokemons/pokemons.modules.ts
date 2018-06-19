99b370a86606a606384b000ed82da6dae4075efe
raphaeldenis <raphael.denis@coding_academy.fr>
2018-06-19 11:09:54 +0200

    importing specific routes to the PokemonModule through PokemonRoutingModule

--- AngularOC/pokemon-app/app/pokemons/pokemons.modules.ts
+++ AngularOC/pokemon-app/app/pokemons/pokemons.modules.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PokemonRoutingModule } from './pokemons-routing.module';
+
 import { ListPokemonComponent } from './list-pokemon.component';
 import { DetailPokemonComponent } from './detail-pokemon.component';
 import { ShadowCardDirective } from './shadow-card.directive';
@@ -10,7 +12,8 @@ import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
     imports: [
         //BrowserModule was including the CommonModule but since we don'need
         //to start app from this submodule, CommonModule is enough
-        CommonModule
+        CommonModule,
+        PokemonRoutingModule
     ],
     //declarations with elements NOT FROM ROOT MODULE but from
     //specified pokemons module
