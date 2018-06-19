321d44ba4bf9ab64ec98d355b2819dd145d3b143
raphaeldenis <raphael.denis@coding_academy.fr>
2018-06-19 11:17:00 +0200

    imported PokemonsModule into rootmodule

--- AngularOC/pokemon-app/app/app.module.ts
+++ AngularOC/pokemon-app/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { PokemonsModule } from './pokemons/pokemons.module'
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 @NgModule({
-    imports: [BrowserModule, AppRoutingModule ],
+    imports: [
+        //MODULE ORDER ARE VITAL!!! (for routes declaration)
+        BrowserModule,
+        PokemonsModule,
+        AppRoutingModule
+     ],
     //declare component/directive in root app module
     declarations: [ 
         AppComponent,
