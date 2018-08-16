eb928517af67e96ace2e5df6505b42aa181a8a7f
raphaeldenis <rdenisfr@gmail.com>
2018-08-16 11:19:31 +0200

    Splitting component into two: list and detail of heroes Test  ok

--- AngularToH/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ AngularToH/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -10,9 +10,14 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
   heroes =  HEROES;
 
+  selectedHero: Hero;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
